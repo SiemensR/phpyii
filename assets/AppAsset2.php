4c8a9e9f520d8ca2d70448db6768e183743ef8af
siemensr <priman2004@mail.ru>
2018-04-22 20:02:47 +0200

    new css added

---
+++ assets/AppAsset2.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class AppAsset2 extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+      'site2.css',
+    'http://wingman.mediumra.re/assets/css/theme.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
